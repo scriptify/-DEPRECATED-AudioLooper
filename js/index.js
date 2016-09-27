3b196a9021646cf7ea86c378c10de348f55b8d13
scriptify <maximilian.torggler@tuta.io>
2016-09-27 18:23:53 +0200

    Initial commit.

--- js/index.js
+++ js/index.js
@@ -1,41 +1,26 @@
-import createEffects from 'webaudio-effect-units-collection';
+import Chnl from 'webaudio-chnl';
 
-export default class Chnl {
-  input;
-  output;
-  effects;
 
-  constructor(audioCtx) {
-    this.context = audioCtx;
-    this.input = audioCtx.createGain();
-    this.output = audioCtx.createGain();
-    this.effects = createEffects(audioCtx);
-    this.setupGraph();
+export default class AudioChnl extends Chnl {
+
+  audioObj;
+
+  constructor(audioCtx, audioObj) {
+    super(audioCtx);
+    this.audioObj = audioObj;
   }
 
-  setupGraph() {
-    const keys = Object.keys(this.effects);
-    // Connect input to first effect
-    this.input.connect( this.effects[keys[0]] );
-
-    // Connect each effect to his 'neighbour'
-    keys.forEach((key, i) => {
-      const currEffect = this.effects[key];
-      if(i < (keys.length - 1))
-        currEffect.connect(this.effects[keys[i + 1]]);
-
-      if(i > 0)
-        currEffect.disable(); // Disable all effects but the gain (first effect) per default
-    });
-
-    // Connect the last effect to the output
-    this.effects[keys[ keys.length - 1 ]].connect(this.output);
-
-
+  start() {
+    this.audioObj.play();
   }
 
-  connect(node) {
-    this.output.connect(node);
+  stop() {
+    this.pause();
+    this.audioObj.currentTime = 0;
+  }
+
+  pause() {
+    this.audioObj.pause();
   }
 
 }
